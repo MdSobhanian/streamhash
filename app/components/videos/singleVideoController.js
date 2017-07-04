224d621791825a3f7c1ad6c8ede50ea5a25d65e2
Shobana-Chandrasekar <shobana@streamhash.com>
2017-07-04 17:32:45 +0530

    Single video page

---
+++ app/components/videos/singleVideoController.js
@@ -0,0 +1,64 @@
+angular.module('streamViewApp')
+.controller('singleVideoController', ['$scope', '$http', '$rootScope', '$window', '$state', '$stateParams',
+
+	function ($scope, $http, $rootScope, $window, $state, $stateParams) {
+
+		$scope.video = '';
+
+		console.log($scope.video);
+
+		$scope.sub_profile_id = memoryStorage.sub_profile_id;
+
+		$scope.height = $(window).height();
+
+		$.ajax({
+
+			type : "post",
+
+			url : apiUrl + "userApi/singleVideo",
+
+			data : {id : memoryStorage.user_id, token : memoryStorage.access_token, admin_video_id : $stateParams.id},
+
+			async : false,
+
+			success : function (data) {
+
+				if (data.success) {
+
+					$scope.video = data;
+
+				} else {
+
+					UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+					return false;
+				}
+			},
+			error : function (data) {
+
+				UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+			},
+		});
+
+
+		jwplayer.key="M2NCefPoiiKsaVB8nTttvMBxfb1J3Xl7PDXSaw==";
+
+		var playerInstance = jwplayer("video-player");
+
+
+		playerInstance.setup({
+            sources: [{
+                file: $scope.video.main_video,
+              }],
+            // file: "{{$trailerstreamUrl}}",
+            image: $scope.video.video.default_image,
+            width: "100%",
+            aspectratio: "16:9",
+            primary: "flash",
+            autostart : true,
+        });
+
+		console.log($scope.video);
+	}
+]);
