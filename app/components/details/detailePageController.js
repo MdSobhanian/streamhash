3ea14dcc016435adb45450d8a1330fc7d8d7bc7a
Shobana-Chandrasekar <shobana@streamhash.com>
2017-07-05 02:06:54 +0530

    Add / Remove Wishlist

--- app/components/details/detailePageController.js
+++ app/components/details/detailePageController.js
@@ -5,6 +5,113 @@ angular.module('streamViewApp')
 
 	function ($scope, $http, $rootScope, $window, $state, $stateParams, $location) {
 
+
+		$scope.addWishlist = function(id, $index, key) {
+
+				$.ajax({
+
+					type : "post",
+
+					url : apiUrl + "userApi/addWishlist",
+
+					data : {id : memoryStorage.user_id, token : memoryStorage.access_token, admin_video_id : id},
+
+					async : false,
+
+					beforeSend : function() {
+
+						$("#my-list-txt_"+$index+"_"+key).html('<a class="my-list bold"><i class="fa fa-plus my-list-icon"></i><span class="my-list-txt">Adding</span></a>');
+
+					},
+
+					success : function (data) {
+
+						if (data.success) {
+
+							$("#my-list-txt_"+$index+"_"+key).html('<a onclick="angular.element(this).scope().removeWishlist('+data.wishlist_id+', '+id+', '+$index+', '+key+')" class="my-list bold" id="remove-my-list-txt" style="cursor: pointer;">'+
+							    							'<i class="fa fa-check my-list-icon"></i>'+
+							    							'<span class="my-list-txt">My List</span>'+
+							    						'</a>');
+
+						} else {
+
+							UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+							return false;
+						}
+					},
+					error : function (data) {
+
+						UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+					},
+
+					/*complete : function(data) {
+
+						$("#before_loader").hide();
+
+					},*/
+				});
+			}
+
+			$scope.closeDiv = function(index, key) {
+
+				$("#"+index+"_"+key+"_video_drop").fadeOut();
+			}
+
+			$scope.removeWishlist = function(id, admin_video_id, $index, key) {
+
+				$.ajax({
+
+					type : "post",
+
+					url : apiUrl + "userApi/deleteWishlist",
+
+					data : {id : memoryStorage.user_id, token : memoryStorage.access_token, wishlist_id : id},
+
+					async : false,
+
+					beforeSend : function() {
+
+						$("#my-list-txt_"+$index+"_"+key).html('<a class="my-list bold"><i class="fa fa-check my-list-icon"></i><span class="my-list-txt">Removing</span></a>');
+
+					},
+
+					success : function (data) {
+
+						if (data.success) {
+
+							$("#my-list-txt_"+$index+"_"+key).html('<a onclick="angular.element(this).scope().addWishlist('+admin_video_id+', '+$index+', '+key+')" class="my-list bold" style="cursor: pointer;">'+
+							    							'<i class="fa fa-plus my-list-icon"></i>'+
+							    							'<span class="my-list-txt">My List</span>'+
+							    						'</a>');
+
+						} else {
+
+							UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+							return false;
+						}
+					},
+					error : function (data) {
+
+						UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+					},
+
+					/*complete : function(data) {
+
+						$("#before_loader").hide();
+
+					},*/
+				});
+			}
+
+			$scope.closeDiv = function(index, key) {
+
+				$("#"+index+"_"+key+"_overview").fadeOut();
+			}
+
 		$.ajax({
 
 			type : "post",
