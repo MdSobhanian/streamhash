8ec00561c8d13e7e5b90297f1610fc7b64857c59
Shobana-Chandrasekar <shobana@streamhash.com>
2017-07-05 16:23:21 +0530

    Issue Fixed - Footer

--- app/components/landing_page/landingController.js
+++ app/components/landing_page/landingController.js
@@ -8,6 +8,18 @@ angular.module('streamViewApp')
 		$scope.site_logo = ($rootScope.site_settings) ? (($rootScope.site_settings[1] != undefined) ? $rootScope.site_settings[1]  : '' ): '';
 
 		$scope.allPages = $rootScope.allPages;
+
+		$scope.user_id = (memoryStorage.user_id != '' && memoryStorage.user_id != undefined ) ? true : false;
+
+		if ($scope.user_id) {
+
+			$state.go('profile.home',{sub_id : memoryStorage.sub_profile_id},{reload:true});
+
+		} else {
+
+			// $state.go('static.index',{},{reload:true});
+
+		}
 		
 	}
 
