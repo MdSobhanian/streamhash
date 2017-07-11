4014d3904208325adff78d254f4eb32dde2ad1a2
Shobana-Chandrasekar <shobana@streamhash.com>
2017-07-11 12:08:54 +0530

    Footer Issue fixed

--- app/components/settings/settingsController.js
+++ app/components/settings/settingsController.js
@@ -5,6 +5,8 @@ angular.module('streamViewApp')
 
 		$rootScope.$emit('navBar', 'black-background');
 
+		
+
 		$scope.login_bg = ($rootScope.site_settings) ? (($rootScope.site_settings[47] != undefined) ? $rootScope.site_settings[47].value  : '' ): '';
 
 		// $rootScope.$emit('footerBar', 'true');
