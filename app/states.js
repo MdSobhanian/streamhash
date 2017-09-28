14dce8925b7c17a0f0b47b63874cc87a220c23d5
Shobana-Chandrasekar <shobana@streamhash.com>
2017-09-28 16:47:11 +0530

    Issues fixed

--- app/states.js
+++ app/states.js
@@ -510,22 +510,6 @@ streamViewApp
                 })
 
 
-
-                .state('profile.test', {
-                    cache: false,
-                    url: "/test",
-                    templateUrl: 'app/components/settings/test.html',
-
-                })
-
-                .state('profile.test_1', {
-                    cache: false,
-                    url: "/test_1",
-                    templateUrl: 'app/components/settings/test_1.html',
-
-                })
-
-
             $httpProvider.interceptors.push('authInterceptor');
 
         }
