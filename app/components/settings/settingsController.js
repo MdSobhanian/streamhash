905dc7655696bfedaee32fc84061d870ba140a7e
Shobana-Chandrasekar <shobana@streamhash.com>
2018-07-03 17:49:17 +0530

    SEO Settings for all page header + Subscription Auto Renewal ( Pause + start subscription for current subscription )

--- app/components/settings/settingsController.js
+++ app/components/settings/settingsController.js
@@ -897,6 +897,51 @@ angular.module('streamViewApp')
 	    		}
 		    }
 
+		    $scope.enable_subscription = function() {
+
+	    		if(confirm('Do you want to enable Auto subscription ?')) {
+
+	    			$.ajax({
+
+						type : "post",
+
+						url : apiUrl + "userApi/autorenewal/enable",
+
+						data : {id : memoryStorage.user_id, token : memoryStorage.access_token},
+
+						async : false,
+					
+
+						success : function (data) {
+
+							if (data.success) {
+
+								UIkit.notify({message : data.message, timeout : 3000, pos : 'top-center', status : 'success'});
+
+								$state.reload();
+
+							} else {
+
+								console.log(data.message);
+
+								UIkit.notify({message : data.error_messages, timeout : 3000, pos : 'top-center', status : 'danger'});
+
+								return false;
+							}
+						},
+						error : function (data) {
+
+							UIkit.notify({message : 'Something Went Wrong, Please Try again later', timeout : 3000, pos : 'top-center', status : 'danger'});
+
+						},
+
+				});
+
+
+
+	    		}
+		    }
+
 
 			$.ajax({
 
