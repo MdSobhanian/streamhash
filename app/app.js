b6361d3844f4478507d92082761362ee259ec9b0
Maheswari Selvan <maheswari@codegama.com>
2019-06-14 18:38:40 +0530

    Frontend language symbol changed
    Summary: 	Symbol changed
    
    Test Plan: 	Streamview frontend & symbol translation changed
    
    Reviewers: vithya, Aravinth
    
    Differential Revision: http://review.botfingers.com/D480

--- app/app.js
+++ app/app.js
@@ -53,13 +53,13 @@ var streamViewApp = angular.module("streamViewApp", [
   "pascalprecht.translate"
 ]);
 
-var route_url = "http://demo.streamhash.com/#";
+var route_url = "http://localhost/streamview-angularjs-base/#";
 
-var apiUrl = "http://adminview.streamhash.com/";
+var apiUrl = "http://localhost:8000/";
 
-var angularUrl = "http://demo.streamhash.com/#/";
+var angularUrl = "http://localhost/streamview-angularjs-base/#/";
 
-var common_url = "http://demo.streamhash.com/assets/";
+var common_url = "http://localhost/streamview-angularjs-base/assets/";
 
 streamViewApp.run([
   "$rootScope",
